981e629dcede2de51bc04681e119ac6c4893c717
aomezzz <aomezzzz.aomsin@gmail.com>
2026-07-07 23:41:38 +0700

    second commit

--- tests/bmi.spec.ts
+++ tests/bmi.spec.ts
@@ -1,19 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('bmi test calculator', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('https://www.calculator.net/bmi-calculator.html');
   await page.locator('#cage').click();
-  await page.locator('#cage').click();
-  await page.locator('#cage').fill('21');
-  await page.getByText('Female').click();
-  await page.locator('label:nth-child(2) > .rbmark').click();
+  await page.locator('#cage').fill('23');
   await page.locator('#cheightmeter').click();
-  await page.locator('#cheightmeter').fill('16');
-  await page.locator('#cheightmeter').press('PageUp');
   await page.locator('#cheightmeter').fill('161');
   await page.locator('#ckg').click();
   await page.locator('#ckg').fill('51');
   await page.getByRole('button', { name: 'Calculate' }).click();
-  const result = await page.locator('div.bigtext').innerText();
-  console.log("ACTUAL RESULT:", result);
+  await expect(page.locator('svg')).toContainText('BMI = 19.7');
 });
